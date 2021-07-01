3aabd4f41be4f7b6ce84239bb7b137c9160c3050
Mathieu <tsunoll96@gmail.com>
2021-07-01 10:30:24 +0200

    Add itowns in IMUV

--- Imuv/client/src/GameApp/GameApp.js
+++ Imuv/client/src/GameApp/GameApp.js
@@ -1,6 +1,6 @@
 /** @format */
 
-import { Game } from 'ud-viz';
+import { Game, itowns } from 'ud-viz';
 import { GameView } from 'ud-viz/src/View/GameView/GameView';
 import { MenuAvatarView } from '../MenuAvatar/MenuAvatar';
 import Constants from 'ud-viz/src/Game/Shared/Components/Constants';
@@ -8,6 +8,10 @@ import WorldStateDiff from 'ud-viz/src/Game/Shared/WorldStateDiff';
 import WorldState from 'ud-viz/src/Game/Shared/WorldState';
 
 import './GameApp.css';
+import {
+  LayerManager,
+  TilesManager,
+} from 'ud-viz/src/Widgets/Components/Components';
 
 export class GameApp {
   constructor(webSocketService, assetsManager, config) {
@@ -39,6 +43,79 @@ export class GameApp {
       const state = new WorldState(json.state);
       _this.worldStateInterpolator.onFirstState(state);
       _this.gameView.onFirstState(state, json.avatarUUID);
+
+      addBaseMapLayer();
+      addElevationLayer();
+      add3DTilesLayer();
+    };
+
+    /**
+     * Adds WMS elevation Layer of Lyon in 2012 and WMS imagery layer of Lyon in 2009 (from Grand Lyon data).
+     */
+    const addBaseMapLayer = function () {
+      let wmsImagerySource = new itowns.WMSSource({
+        extent: _this.gameView.extent,
+        name: _this.config['background_image_layer']['name'],
+        url: _this.config['background_image_layer']['url'],
+        version: _this.config['background_image_layer']['version'],
+        projection: _this.config['projection'],
+        format: _this.config['background_image_layer']['format'],
+      });
+
+      // Add a WMS imagery layer
+      let wmsImageryLayer = new itowns.ColorLayer(
+        _this.config['background_image_layer']['layer_name'],
+        {
+          updateStrategy: {
+            type: itowns.STRATEGY_DICHOTOMY,
+            options: {},
+          },
+          source: wmsImagerySource,
+          transparent: true,
+        }
+      );
+      _this.gameView.getItownsView().addLayer(wmsImageryLayer);
+    };
+
+    const addElevationLayer = function () {
+      // Add a WMS elevation source
+      let wmsElevationSource = new itowns.WMSSource({
+        extent: _this.gameView.extent,
+        url: _this.config['elevation_layer']['url'],
+        name: _this.config['elevation_layer']['name'],
+        projection: _this.config['projection'],
+        heightMapWidth: 256,
+        format: _this.config['elevation_layer']['format'],
+      });
+      // Add a WMS elevation layer
+      let wmsElevationLayer = new itowns.ElevationLayer(
+        _this.config['elevation_layer']['layer_name'],
+        {
+          useColorTextureElevation: true,
+          colorTextureElevationMinZ: 144,
+          colorTextureElevationMaxZ: 622,
+          source: wmsElevationSource,
+        }
+      );
+      _this.gameView.getItownsView().addLayer(wmsElevationLayer);
+    };
+
+    const add3DTilesLayer = function () {
+      const $3DTilesLayer = new itowns.C3DTilesLayer(
+        _this.config['3DTilesLayer']['id'],
+        {
+          name: 'Lyon-2015-'.concat(_this.config['3DTilesLayer']['id']),
+          source: new itowns.C3DTilesSource({
+            url: _this.config['3DTilesLayer']['url'],
+          }),
+        },
+        _this.gameView.getItownsView()
+      );
+
+      itowns.View.prototype.addLayer.call(
+        _this.gameView.getItownsView(),
+        $3DTilesLayer
+      );
     };
 
     // Register callbacks
@@ -119,6 +196,7 @@ export class GameApp {
         document.body.appendChild(menuAvatar.html());
       };
     }
+
 
     onLoad();
   }
